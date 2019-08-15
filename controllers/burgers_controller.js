0777a87b9e996d2b8714bc1208805f3f8cd143d5
Flying Serpent <nickprather11@gmail.com>
2019-08-15 16:44:26 -0400

    burger names now displaying properly. changed {{name}} in index.handlebars to {{burger_name}}

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -14,7 +14,7 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burger.create(req.body.name, req.body.devoured, function(result) {
-    // Send back the ID of the new quote
+    // Send back the ID of the new burger
     res.json({ id: result.insertId });
   });
 });

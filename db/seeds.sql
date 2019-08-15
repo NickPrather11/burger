3eb7ce78606204b531f409e32c184e1bbd07462d
Flying Serpent <nickprather11@gmail.com>
2019-08-14 22:37:13 -0400

    server.js, orm.js, burger.js, controller, schema, and seeds all populated but UNTESTED. Handlebars files started: main is done, index needs more work

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Romanes of the Day Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Yes I Cayenne Burger", false);
+
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("Sweet Home Avocado Burger", false);
---
+++ server.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+
+var app = express();
+
+var PORT = process.env.PORT || 8080;
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
